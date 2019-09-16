57de59c585a471b0217da3ecdcbed20fb1f78243
pengw <wenjia.peng@siemens.com>
2019-09-15 20:21:44 -0400

    fix unit test

--- test/few-button.test.js
+++ test/few-button.test.js
@@ -1,7 +1,7 @@
 // NOTE: Example code from google, will be deleted later
 /* eslint-env es6, jasmine */
 
-import { FewButton } from '../src/few-button';
+import FewButton from '../src/few-button';
 import { TestUtils } from './test-utils';
 
 describe( 'Test few-button', () => {
