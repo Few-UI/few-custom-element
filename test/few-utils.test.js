d4847ef46074ced5a13d0880d292d7c6c959e651
jesse23 <pengwenjia@gmail.com>
2019-09-26 14:04:56 -0400

    fix unit test

--- test/few-utils.test.js
+++ test/few-utils.test.js
@@ -1,6 +1,6 @@
 /* eslint-env es6, jasmine */
 
-import utils from '../src/few-utils';
+import { parseView } from '../src/few-utils';
 
 describe( 'Test few-utils', () => {
   it( 'Verify parseView can parse DOM correctly', () => {
@@ -8,7 +8,7 @@ describe( 'Test few-utils', () => {
         '<button>Button1</button>' +
         '<button>Button2</button>';
 
-    expect( utils.parseView( viewHtml ).outerHTML ).toEqual(
+    expect( parseView( viewHtml ).outerHTML ).toEqual(
         '<div><button>Button1</button><button>Button2</button></div>' );
   } );
 } );
