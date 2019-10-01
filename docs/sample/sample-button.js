de6ed234fc17c661e5d63697a48dfc2a5f763926
jesse23 <pengwenjia@gmail.com>
2019-10-01 08:23:50 -0400

    beautify

--- docs/sample/sample-button.js
+++ docs/sample/sample-button.js
@@ -1,35 +1,35 @@
 /* eslint-env es6 */
 
 define( [], () => {
-class FewButton extends HTMLElement {
-    static get tag() {
-        return 'sample-button';
+    class FewButton extends HTMLElement {
+        static get tag() {
+            return 'sample-button';
+        }
+
+        get clickAction() {
+            return this.getAttribute( 'click' );
+        }
+
+        constructor() {
+            super();
+
+            const shadowRoot = this.attachShadow( { mode: 'open' } );
+
+            // Apply style to shadow DOM
+            // let style = document.createElement( 'style' );
+            // style.textContent = buttonCss;
+            // shadowRoot.appendChild( style );
+
+            //let newDom = document.createElement( 'div' );
+            shadowRoot.innerHTML = '<button class="base-button" ><slot/></button>';
+        }
+
+        /*
+        Cannot do it here - need to load context from parent
+        attributeChangedCallback( name, oldValue, newValue ) {
+            console.log( `${name}: ${oldValue} => ${newValue}` );
+        }
+        */
     }
-
-    get clickAction() {
-        return this.getAttribute( 'click' );
-    }
-
-    constructor() {
-        super();
-
-        const shadowRoot = this.attachShadow( { mode: 'open' } );
-
-        // Apply style to shadow DOM
-        // let style = document.createElement( 'style' );
-        // style.textContent = buttonCss;
-        // shadowRoot.appendChild( style );
-
-        //let newDom = document.createElement( 'div' );
-        shadowRoot.innerHTML = '<button class="base-button" ><slot/></button>';
-    }
-
-    /*
-    Cannot do it here - need to load context from parent
-    attributeChangedCallback( name, oldValue, newValue ) {
-        console.log( `${name}: ${oldValue} => ${newValue}` );
-    }
-    */
-}
-customElements.define( FewButton.tag, FewButton );
+    customElements.define( FewButton.tag, FewButton );
 } );
