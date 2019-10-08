e4c28f283aaa8b096a2ad37b3cd3aa01a4120efc
jesse23 <pengwenjia@gmail.com>
2019-10-08 16:34:17 -0400

    remove dependency on few-bridge

--- docs/main.js
+++ docs/main.js
@@ -1,7 +1,6 @@
 // main entrance for debug test site
 /* eslint-env es6 */
 import '../src/few-view-element';
-import '../src/few-bridge';
 import '../src/popup-info';
 
 // even though Rollup is bundling all your files together, errors and
