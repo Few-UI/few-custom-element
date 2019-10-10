3385757d502bf7de9fdfbc0697c5c885174cbd35
jesse23 <pengwenjia@gmail.com>
2019-10-10 17:01:53 -0400

    refactor

--- docs/bundle.js
+++ docs/bundle.js
@@ -22089,88 +22089,76 @@ define(['require'], function (require) { 'use strict';
 
   // Create a class for the element
   class PopUpInfo extends HTMLElement {
-    get text() {
-        return this.getAttribute( 'text' );
-    }
-
-    constructor() {
-      // Always call super first in constructor
-      super();
-
-      // Create a shadow root
-      const shadow = this.attachShadow( { mode: 'open' } );
-
-      // Create spans
-      const wrapper = document.createElement( 'span' );
-      wrapper.setAttribute( 'class', 'wrapper' );
-
-      const icon = document.createElement( 'span' );
-      icon.setAttribute( 'class', 'icon' );
-      icon.setAttribute( 'tabindex', 0 );
-
-      const info = document.createElement( 'span' );
-      info.setAttribute( 'class', 'info' );
-
-      // Take attribute content and put it inside the info span
-      // NOTE: without polyfill do it here is useless, not getting the attribute here
-      let text = this.getAttribute( 'text' );
-      text = this.text;
-      info.textContent = text;
-      this.__info = info;
-
-      // Insert icon
-      let imgUrl;
-      if( this.hasAttribute( 'img' ) ) {
-        imgUrl = this.getAttribute( 'img' );
-      } else {
-        imgUrl = 'img/default.png';
+      get text() {
+          return this.getAttribute( 'text' );
       }
 
-      const img = document.createElement( 'img' );
-      img.src = imgUrl;
-      icon.appendChild( img );
+      constructor() {
+          // Always call super first in constructor
+          super();
 
-      // Create some CSS to apply to the shadow dom
-      const style = document.createElement( 'style' );
-      console.log( style.isConnected );
+          // Create a shadow root
+          const shadow = this.attachShadow( { mode: 'open' } );
 
-      style.textContent = `
-      .wrapper {
-        position: relative;
-      }
-      .info {
-        font-size: 0.8rem;
-        width: 200px;
-        display: inline-block;
-        border: 1px solid black;
-        padding: 10px;
-        background: white;
-        border-radius: 10px;
-        opacity: 0;
-        transition: 0.6s all;
-        position: absolute;
-        bottom: 20px;
-        left: 10px;
-        z-index: 3;
-      }
-      img {
-        width: 1.2rem;
-      }
-      .icon:hover + .info, .icon:focus + .info {
-        opacity: 1;
-      }
-    `;
+          // Create spans
+          const wrapper = document.createElement( 'span' );
+          wrapper.setAttribute( 'class', 'wrapper' );
 
-      // Attach the created elements to the shadow dom
-      shadow.appendChild( style );
-      console.log( style.isConnected );
-      shadow.appendChild( wrapper );
-      wrapper.appendChild( icon );
-      wrapper.appendChild( info );
-    }
+          const mainDom = document.createElement( 'code' );
+          mainDom.classList.add( 'few-popup' );
+          mainDom.appendChild( document.createElement( 'slot' ) );
+
+          const info = document.createElement( 'span' );
+          info.setAttribute( 'class', 'info' );
+
+          // Take attribute content and put it inside the info span
+          // NOTE: without polyfill do it here is useless, not getting the attribute here
+          let text = this.getAttribute( 'msg' );
+          text = this.text;
+          info.textContent = text;
+          this.__info = info;
+
+          // Create some CSS to apply to the shadow dom
+          const style = document.createElement( 'style' );
+
+          style.textContent = `
+          .wrapper {
+            position: relative;
+          }
+          .info {
+            font-size: 0.8rem;
+            width: 200px;
+            display: inline-block;
+            border: 1px solid black;
+            padding: 10px;
+            background: white;
+            border-radius: 10px;
+            opacity: 0;
+            transition: 0.6s all;
+            position: absolute;
+            bottom: 20px;
+            left: 10px;
+            z-index: 3;
+          }
+          img {
+            width: 1.2rem;
+          }
+          .few-popup:hover + .info, .few-popup:focus + .info {
+            opacity: 1;
+          }
+        `;
+
+          // wrapper
+          wrapper.appendChild( mainDom );
+          wrapper.appendChild( info );
+
+          // Attach the created elements to the shadow dom
+          shadow.appendChild( style );
+          shadow.appendChild( wrapper );
+      }
 
       connectedCallback() {
-        this.__info.textContent = this.getAttribute( 'text' );
+          this.__info.textContent = this.getAttribute( 'msg' );
       }
   }
 
