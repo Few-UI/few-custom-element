2afdf55fd11cc8420a4500c20dd31153b6ba0a2b
jesse23 <pengwenjia@gmail.com>
2019-10-10 17:02:53 -0400

    indent

--- docs/bundle.js
+++ docs/bundle.js
@@ -22122,30 +22122,30 @@ define(['require'], function (require) { 'use strict';
           const style = document.createElement( 'style' );
 
           style.textContent = `
-          .wrapper {
-            position: relative;
-          }
-          .info {
-            font-size: 0.8rem;
-            width: 200px;
-            display: inline-block;
-            border: 1px solid black;
-            padding: 10px;
-            background: white;
-            border-radius: 10px;
-            opacity: 0;
-            transition: 0.6s all;
-            position: absolute;
-            bottom: 20px;
-            left: 10px;
-            z-index: 3;
-          }
-          img {
-            width: 1.2rem;
-          }
-          .few-popup:hover + .info, .few-popup:focus + .info {
-            opacity: 1;
-          }
+            .wrapper {
+                position: relative;
+            }
+            .info {
+                font-size: 0.8rem;
+                width: 200px;
+                display: inline-block;
+                border: 1px solid black;
+                padding: 10px;
+                background: white;
+                border-radius: 10px;
+                opacity: 0;
+                transition: 0.6s all;
+                position: absolute;
+                bottom: 20px;
+                left: 10px;
+                z-index: 3;
+            }
+            img {
+                width: 1.2rem;
+            }
+            .few-popup:hover + .info, .few-popup:focus + .info {
+                opacity: 1;
+            }
         `;
 
           // wrapper
