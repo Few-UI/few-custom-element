ea8cc14e2847fae4851b8377b8288850b097fb6b
jesse23 <pengwenjia@gmail.com>
2019-10-25 22:27:56 -0400

    code clean up

--- docs/steal.config.js
+++ docs/steal.config.js
@@ -1,10 +1,9 @@
-// config.js
+/* eslint-env es6, node */
 /*
  global
  steal,
  */
 steal.config( {
-    // paths: {"jquery": "http://code.jquery.com/jquery-1.11.0.min.js"}
     // current stealjs has a bug - baseURL will be overwritten by the path of steal.config
     // Sould be able to play it with config but let us not over engineering this for now
     // baseURL: '../lib/',
